33088c60931eca21438567063c085415ca8a17c9
Ahmed <ahmedzon0598596843@gmail.com>
2023-08-02 01:49:16 +0300

    learn - Getx

--- lib/screen/main.dart
+++ lib/screen/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:zon/screen/test1.dart';
 
 import '../contral/homecontral.dart';
 import '../core/resources/manager/font.dart';
@@ -9,83 +10,105 @@ import '../core/resources/manager/manager_fontweight.dart';
 import '../core/resources/manager/manager_strings.dart';
 
 class mainazkar extends StatelessWidget {
-  const mainazkar({super.key});
+  mainazkar({super.key});
+
+  contral controller = Get.put(contral(),permanent: true);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        floatingActionButton: GetBuilder<contral>(
-          init: contral(),
-          builder: (controller) => FloatingActionButton(
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+          floatingActionButton: FloatingActionButton(
+              heroTag: "btn2",
               backgroundColor: Colors.black,
               child: Icon(Icons.add, color: Colors.white),
               onPressed: () {
                 controller.plass();
               }),
-        ),
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          actions: [],
-          title: const Text(
-            Manager_string.S_Azkar,
-            style: TextStyle(
-                fontSize: Manager_fontsizes.siz33,
-                fontWeight: Manager_fontweight.font_w600,
-                fontFamily: font.font_ReemKufiFun),
+          extendBodyBehindAppBar: true,
+          appBar: AppBar(
+            actions: [],
+            title: const Text(
+              Manager_string.S_Azkar,
+              style: TextStyle(
+                  fontSize: Manager_fontsizes.siz33,
+                  fontWeight: Manager_fontweight.font_w600,
+                  fontFamily: font.font_ReemKufiFun),
+            ),
+            centerTitle: true,
+            backgroundColor: ColorsApp.transparent,
           ),
-          centerTitle: true,
-          backgroundColor: ColorsApp.transparent,
-        ),
-        body: Stack(
-          children: [
-            Container(
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: AssetImage("lib/core/resources/images/img.png"),
-                    fit: BoxFit.cover),
+          body: Stack(
+            children: [
+              Container(
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("lib/core/resources/images/img.png"),
+                      fit: BoxFit.cover),
+                ),
               ),
-            ),
-            Align(
-              child: Container(
-                  decoration: BoxDecoration(
-                      color: Colors.black45,
-                      borderRadius: BorderRadius.circular(5)),
-                  alignment: Alignment.center,
-                  height: 50,
-                  width: 50,
-                  child: Center(
-                    child: GetBuilder<contral>(
-                      init: contral(),
-                      builder: (controller) => Text(
-                        "${controller.number}",
-                        style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: Manager_fontweight.font_w600,
-                            color: Colors.white),
+              Align(
+                child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.black45,
+                        borderRadius: BorderRadius.circular(5)),
+                    alignment: Alignment.center,
+                    height: 50,
+                    width: 50,
+                    child: Center(
+                      child: GetBuilder<contral>(
+                          init:contral() ,
+                        builder: (controller) => Text(
+                          "${controller.number}",
+                          style: TextStyle(
+                              fontSize: 30,
+                              fontWeight: Manager_fontweight.font_w600,
+                              color: Colors.white),
+                        ),
                       ),
-                    ),
-                  )),
-            ),
-            GetBuilder<contral>(
-                  init: contral(),
-                  builder: (controller) => InkWell(
-                    onTap: (){
+                    )),
+              ),
+              InkWell(
+                onTap: () {
+                  print("object");
+                },
+                child: Container(
+                    alignment: AlignmentDirectional.bottomStart,
+                    margin: EdgeInsetsDirectional.only(bottom: 14, start: 14),
+                    child: FloatingActionButton(
+                      heroTag: "btn1",
+                      child: Icon(
+                        Icons.remove,
+                        color: Colors.white,
+                      ),
+                      backgroundColor: Colors.black,
+                      onPressed: () {
+                        controller.mains();
+                      },
+                    )),
+              ),
+              InkWell(
+                onTap: () {
+                  print("object");
+                },
+                child: Container(
+                    alignment: AlignmentDirectional.bottomStart,
+                    margin: EdgeInsetsDirectional.only(bottom: 150, start: 14),
+                    child: FloatingActionButton(
+                      child: Icon(
+                        Icons.arrow_back_ios_new,
+                        color: Colors.white,
+                      ),
+                      backgroundColor: Colors.black,
+                      onPressed: () {
+                        Get.off(()=>test1());
 
-                      print("object");
-                    },
-                    child: Container(
-                        alignment: AlignmentDirectional.bottomStart,
-                        margin: EdgeInsetsDirectional.only(bottom: 14,start: 14),
-                        child: FloatingActionButton(
-                          child: Icon(Icons.remove,color: Colors.white,),
-                          backgroundColor: Colors.black,
-                          onPressed: () {
-                            controller.mains();
-                          },
-                        )),
-
-                ))
-          ],
-        ));
+                      },
+                    )),
+              ),
+            ],
+          )),
+    );
   }
 }
